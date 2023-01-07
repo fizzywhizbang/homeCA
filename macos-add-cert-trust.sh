35c9ea3514c1a3fec059beb1653502ea87818f22
FizzyWhizbang <marc.levine@gmail.com>
2023-01-07 13:49:48 -0600

    Update macos-add-cert-trust.sh
    missing or

--- macos-add-cert-trust.sh
+++ macos-add-cert-trust.sh
@@ -1,7 +1,7 @@
 read -p "Enter the name of the CA to add to your trust store:" CA
 read -p "Is this an intermediate or root (I or R)" OPT
 
-if [ $OPT == "I" ] [ $OPT == "i" ]
+if [ $OPT == "I" ] || [ $OPT == "i" ]
 then
     sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "interemdiate/${CA}_sig.pem"
 else
